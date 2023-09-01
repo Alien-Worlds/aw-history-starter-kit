afaa459c6a16f5e1a57faa37008f0bbddc543584
rkamysz <radoslaw.kamysz@gmail.com>
2023-09-01 13:14:07 +0200

    update

--- src/reader/reader.dependencies.ts
+++ src/reader/reader.dependencies.ts
@@ -1,5 +1,7 @@
 import {
   BlockRangeScanner,
+  BlockReader,
+  BlockState,
   BroadcastClient,
   BroadcastTcpClient,
   DatabaseConfigBuilder,
@@ -7,10 +9,20 @@ import {
   ReaderConfig,
   ReaderDependencies,
   Result,
+  UnprocessedBlockQueue,
 } from '@alien-worlds/aw-history';
-import { MongoConfig, buildMongoConfig } from '@alien-worlds/aw-storage-mongodb';
-import { BlockRangeScannerFactory } from '../common';
+import {
+  MongoConfig,
+  MongoSource,
+  buildMongoConfig,
+} from '@alien-worlds/aw-storage-mongodb';
+import {
+  BlockRangeScannerFactory,
+  BlockStateFactory,
+  UnprocessedBlockQueueFactory,
+} from '../common';
 import path from 'path';
+import { AntelopeBlockReaderFactory } from '@alien-worlds/aw-antelope';
 
 /**
  * An abstract class representing a reader dependencies.
@@ -19,6 +31,10 @@ import path from 'path';
 export class DefaultReaderDependencies implements ReaderDependencies {
   public broadcastClient: BroadcastClient;
   public scanner: BlockRangeScanner;
+  public blockState: BlockState;
+  public unprocessedBlockQueue: UnprocessedBlockQueue;
+  public blockReader: BlockReader;
+
   public databaseConfigBuilder: DatabaseConfigBuilder = buildMongoConfig;
 
   public readonly workerLoaderDependenciesPath = path.join(
@@ -28,11 +44,22 @@ export class DefaultReaderDependencies implements ReaderDependencies {
 
   public async initialize(config: ReaderConfig<MongoConfig>): Promise<Result> {
     try {
+      const mongo = await MongoSource.create(config.database);
       this.broadcastClient = new BroadcastTcpClient(config.broadcast, 'reader');
       this.scanner = await BlockRangeScannerFactory.create(
         config.database,
         config.scanner
       );
+      this.blockReader = await AntelopeBlockReaderFactory.create(
+        mongo,
+        config.blockReader
+      );
+      this.blockState = await BlockStateFactory.create(mongo);
+      this.unprocessedBlockQueue = await UnprocessedBlockQueueFactory.create(
+        mongo,
+        config.unprocessedBlockQueue
+      );
+
       return Result.withoutContent();
     } catch (error) {
       return Result.withFailure(Failure.fromError(error));
